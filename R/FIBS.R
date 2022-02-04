4b5ad0ad8dc72b17413195d62765d1238d16c69b
Elizabeth Stroud <elizabethastroud@icloud.com>
2022-02-04 14:06:10 +0000

    First Commit and first attempt at using Git...

---
+++ R/FIBS.R
@@ -0,0 +1,158 @@
+#FIBSLDA package
+#remember input csv for model.lda() needs to be in the order
+#"SLA","ARNODE","LOGCAHN","LOGCADN","FLOWPER","Study"
+
+#model.LDA() will take the functional attribute scores of archeaobotanical samples and compared them to a linear discriminant analysis
+#of known crop husbandry regimes and classify the archaeobotanical samples in to either low intensity/extensive or high intensity/intensive
+#cultivation. Currently two model types are available - the "temperate" model is suitable for temperate locations, while the "arid" model is
+#suitable for semi-arid locations
+model.LDA<-function(model,x){
+  library(dplyr)
+  library(haven)
+  library(MASS)
+  if (model=='temperate') load(file="data_model.rda")
+  if(model=='arid')load (file="data_model_arid.rda")
+  discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model, CV = TRUE)
+  model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model)
+  predictionmodel <- predict(model_lda,data.model)
+  functionalAt <- data.frame(Study = as.factor(data.model$Study),
+                             Classification= predictionmodel$class,
+                             LD1 = predictionmodel$x)
+  centroids <- functionalAt %>%
+    group_by(Study) %>%
+    summarise(centroid1 = mean(LD1))
+  model <- cbind(as.data.frame(predict(model_lda,x)),x)
+  print(model)
+}
+
+### this function uses the package beeswarm to plot the resulting discriminate functions from model.LDA.
+#model is the model used to classify the data - either temperate or arid
+#x is the column which contains the LD1 data of the archaeological samples from model.LDA
+#xlims are the limits of the x axis
+# method follows beeswarm method
+#pch1,and col1 are the colour and symbol used to denote the centroids of the model
+#pch2,col2 are the colour and symbol used to represent the modern model used
+#pch3,col3 are the colour and symbol used to represent the archaeological data used
+plot1<-function(model,x, xlims,method,pch1, pch2, pch3, col1, col2, col3){
+  library(beeswarm)
+  library(dplyr)
+  library(haven)
+  library(MASS)
+  if (model=='temperate') load(file="data_model.rda")
+  if(model=='arid')load (file="data_model_arid.rda")
+  discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model, CV = TRUE)
+  model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model)
+  predictionmodel <- predict(model_lda,data.model)
+  functionalAt <- data.frame(Study = as.factor(data.model$Study),
+                             Classification= predictionmodel$class,
+                             LD1 = predictionmodel$x)
+  centroids <- functionalAt %>%
+    group_by(Study) %>%
+    summarise(centroid1 = mean(LD1))
+
+  par(mar =c(12,2,1,2))
+  beeswarm::beeswarm(predictionmodel$x*-1, vertical =FALSE,side=1, ylim = c(0.85,2), method=method, xlim=xlims, col=col2,pch=pch2, axes=F)
+  par(new=T)
+  beeswarm::beeswarm(centroids$centroid1*-1, vertical =FALSE, ylim=c(-0.2,3), side=-1,method=method, xlim=xlims,col=col1, pch=pch1, axes=F)
+  par(new=T)
+  beeswarm::beeswarm(x*-1, vertical =FALSE,  ylim= c(1,1.5),side=1,method=method, xlim=xlims, col=col3, pch=pch3, axes=F)
+  axis(side=1)
+}
+
+#this function will plot just the centroids of the modern model and the archaeological datapoints
+#all variables follow plot1
+plot2<-function(model, x, xlims,method, pch1= NULL,  pch3= NULL, col1= NULL, col3= NULL){
+  library(beeswarm)
+  library(dplyr)
+  library(haven)
+  library(MASS)
+  if (model=='temperate') load(file="data_model.rda")
+  if(model=='arid')load (file="data_model_arid.rda")
+  discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model, CV = TRUE)
+  model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model)
+  predictionmodel <- predict(model_lda,data.model)
+  functionalAt <- data.frame(Study = as.factor(data.model$Study),
+                             Classification= predictionmodel$class,
+                             LD1 = predictionmodel$x)
+  centroids <- functionalAt %>%
+    group_by(Study) %>%
+    summarise(centroid1 = mean(LD1))
+  beeswarm::beeswarm(centroids$centroid1*-1, vertical =FALSE, side=-1,method=method, xlim=xlims,col=col1, pch=pch1, axes=F)
+  par(new=T)
+  beeswarm::beeswarm(x*-1, vertical =FALSE,  ylim= c(1,1.05),side=1,method=method, xlim=xlims, col=col3, pch=pch3,axes=F)
+  axis(side=1)
+}
+
+#this function uses beewarm's swarmy function to plot the same varbiables as plot 2
+#df is the dataframe of archaeological results
+# x is the column of df which as the LD1 results
+#xlim is the limits of the x axis, while ticks is the location of the x axis labels
+#col 1 and pch 1 = centroids, col3 and pch3 is the colour and symbol of the archaeological data, col2 and pch2 are the model data
+# priority can be ascending,descending,density or random, compact can be True or false - this are from beeswarm swarmy and change the look of the graphs
+
+plot3<-function(model,df, x, xlims,ticks, col1, col2,col3, pch1, pch2, pch3, compact, priority){
+  if (model=='temperate') load(file="data_model.rda")
+  if(model=='arid')load (file="data_model_arid.rda")#### I have a fun feeling this may not work ( unless I make sure it is called data.model (internally))
+  discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model, CV = TRUE)
+  model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model)
+  predictionmodel <- predict(model_lda,data.model)
+  functionalAt <- data.frame(Study = as.factor(data.model$Study),
+                             Classification= predictionmodel$class,
+                             LD1 = predictionmodel$x)
+  centroids <- functionalAt %>%
+    group_by(Study) %>%
+    summarise(centroid1 = mean(LD1))
+  par(mar=c(4,0,0,2), xpd=TRUE)
+  plot(2:5, type='n', xlim = xlims, ylim=c(0,0.17),axes=F, xlab = "", ylab="")
+  points(swarmy(centroids$centroid1*-1, rep(0.15,2)), col= col1, pch=pch1, cex=1.2)
+  points(swarmy(predictionmodel$x*-1, rep(0.1,2), side=1, compact=compact, priority = priority),col=col2, pch=pch2,cex=1.2)
+  points(swarmy(x*-1, rep(0.03, nrow(df)), side=1,compact=compact, priority=priority), col= col3, pch=pch3,cex=1.2)
+  axis(1, ticks, cex=1.5)
+
+  legend("topright", inset=c(-0.05,0.05), c("Group \ncentroids", "Model","Archaeological \nsamples"), pch=c(pch1,pch2, pch3), col= c(col1,col2,col3), cex=0.95, bty="n")
+}
+#this function uses beewarm's swarmy function to plot the same varbiables as plot 2
+#df is the dataframe of archaeological results
+#y is the column of df which as the LD1 results
+#xlim is the limits of the x axis, while ticks is the location of the x axis labels
+#col 1 and pch 1 = centroids, col3 and pch3 is the colar and symbol of the archaeological data
+
+
+plot4<-function(model, df, x, xlims,ticks, col1, col3, pch1, pch3, compact, priority){
+  if (model=='temperate') load(file="data_model.rda")
+  if(model=='arid')load (file="data_model_arid.rda")
+  discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model, CV = TRUE)
+  model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model)
+  predictionmodel <- predict(model_lda,data.model)
+  functionalAt <- data.frame(Study = as.factor(data.model$Study),
+                             Classification= predictionmodel$class,
+                             LD1 = predictionmodel$x)
+  centroids <- functionalAt %>%
+    group_by(Study) %>%
+    summarise(centroid1 = mean(LD1))
+
+  plot(2:5, type='n', xlim = xlims, ylim=c(0,0.2),axes=F, xlab = "", ylab="")
+  points(swarmy(centroids$centroid1*-1, rep(0.15,2)), col= col1, pch=pch1, cex=1.2)
+  points(swarmy(x*-1, rep(0.05, nrow(df)), side=1,compact=compact, priority = priority), col= col3, pch=pch3)
+  axis(1, ticks)
+}
+
+plot5<-function(model, df,x,xlims,ticks,col1,col2,col3,pch1,pch2, pch3, compact,priority){
+  if (model=='temperate') load(file="data_model.rda")
+  if(model=='arid')load (file="data_model_arid.rda")
+  discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model, CV = TRUE)
+  model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model)
+  predictionmodel <- predict(model_lda,data.model)
+  functionalAt <- data.frame(Study = as.factor(data.model$Study),
+                             Classification= predictionmodel$class,
+                             LD1 = predictionmodel$x)
+  centroids <- functionalAt %>%
+    group_by(Study) %>%
+    summarise(centroid1 = mean(LD1))
+
+  plot(2:5, type='n', xlim = xlims, ylim=c(0,0.2),axes=F, xlab = "", ylab="")
+  points(swarmy(centroids$centroid1*-1, rep(0.15,2)), col= col1, pch=pch1, cex=1.2)
+  points(swarmy())
+  points(swarmy(x*-1, rep(0.05, nrow(df)), side=1,compact=compact, priority = priority), col= col3, pch=pch3)
+  axis(1, ticks)
+}
