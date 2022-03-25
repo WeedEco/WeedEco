63db80c6c25a64f2e58564415ed35280743e688f
Elizabeth Stroud <elizabethastroud@icloud.com>
2022-03-25 17:27:45 +0000

    editied .Rd docs for functions

--- R/Plot4.R
+++ R/Plot4.R
@@ -10,7 +10,7 @@ plot4<-function(model, x, xlims= NULL, ylims = NULL, ticks =NULL, col1="black",c
   library(haven)
   library(MASS)
 
-  if (model=='model1'|model== 1) load(file="data_model.rda")
+  if(model=='model1'|model== 1) load(file="data_model.rda")
   if(model=='model1'|model== 1) discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model, CV = TRUE)
   if(model=='model1'|model== 1) model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model)
 
