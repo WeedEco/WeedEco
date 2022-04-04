118c739a41b164f86209b687eaa0145ec62d6882
Elizabeth Stroud <elizabethastroud@icloud.com>
2022-04-04 11:38:51 +0100

    changed from load to data

--- R/FIBS.R
+++ R/FIBS.R
@@ -10,20 +10,20 @@ model.LDA<-function(model,x){
   library(dplyr)
   library(haven)
   library(MASS)
-  if (model=='model1'|model== 1) usethis::use_data("model1.rda")
-  if(model=='model1'|model== 1) discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model, CV = TRUE)
-  if(model=='model1'|model== 1) model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data.model)
+  if (model=='model1'|model== 1) data(model1)
+  if(model=='model1'|model== 1) discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data, CV = TRUE)
+  if(model=='model1'|model== 1) model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN+FLOWPER,data)
 
-  if(model=='model2'|model== 2)use(file="model2.rda")
-  if(model=='model2'|model== 2) discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN,data.model, CV = TRUE)
-  if(model=='model2'|model== 2) model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN,data.model)
+  if(model=='model2'|model== 2)data(model2)
+  if(model=='model2'|model== 2) discrim_cv <- lda(Study ~ SLA+ARNODE+LOGCAHN+LOGCADN,data, CV = TRUE)
+  if(model=='model2'|model== 2) model_lda <- lda(Study ~SLA+ARNODE+LOGCAHN+LOGCADN,data)
 
   if(model=='model3'|model== 3)load (file="model3.rda")
-  if(model=='model3'|model== 3) discrim_cv <- lda(Study ~ FLOWPER+VEGPROP,data.model, CV = TRUE)
-  if(model=='model3'|model== 3) model_lda <- lda(Study ~FLOWPER+VEGPROP,data.model)
+  if(model=='model3'|model== 3) discrim_cv <- lda(Study ~ FLOWPER+VEGPROP,data, CV = TRUE)
+  if(model=='model3'|model== 3) model_lda <- lda(Study ~FLOWPER+VEGPROP,data)
 
 
-  predictionmodel <- predict(model_lda,data.model)
+  predictionmodel <- predict(model_lda,data)
   functionalAt <- data.frame(Study = as.factor(data.model$Study),
                              Classification= predictionmodel$class,
                              LD1 = predictionmodel$x)
